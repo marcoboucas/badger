38d2a433b1c3dc841a67cf82f2b365d3a28a37f2
Marco Boucas <marco.boucas@hotmail.fr>
2022-11-11 18:31:27 +0100

    Add PW (should work) + icosn

--- badger-front/src/api/badges.ts
+++ badger-front/src/api/badges.ts
@@ -3,6 +3,5 @@ import { BadgeReport } from "@badger/common";
 export const getBadgesReports = async (): Promise<BadgeReport[]> => {
   const response = await fetch("/badges.json");
   const badges = await response.json();
-  console.log(badges);
   return badges;
 };
--- badger-front/src/components/Appbar/index.tsx
+++ badger-front/src/components/Appbar/index.tsx
@@ -2,13 +2,19 @@ import AppBar from "@mui/material/AppBar";
 import Box from "@mui/material/Box";
 import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
+import BadgerImageUrl from "./icon-192x192.png";
 
 export default function ResponsiveAppBar() {
   return (
     <Box sx={{ flexGrow: 1 }}>
       <AppBar position="static">
         <Toolbar>
-          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+          <img src={BadgerImageUrl} alt="Badger" width="32" height="32" />
+          <Typography
+            variant="h6"
+            component="div"
+            sx={{ flexGrow: 1, marginLeft: 2 }}
+          >
             Badger
           </Typography>
           {/* <Button color="inherit">Login</Button> */}
